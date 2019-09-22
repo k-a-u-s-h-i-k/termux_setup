1a0758730da3aa682e76d6fe27a103a3fd2421b0
Kaushik <kaushik@gmail.com>
2019-09-22 15:19:12 -0400

    Use apt instead of pkg

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# needed to get access to android storage
-termux-setup-storage
+# update repos first
+apt update && apt upgrade
 
 # needed to send notifications to android system. You must have termux-api app downloaded from play store
-pkg install termux-api
+apt install -y termux-api
 
 # update the repos and install python ffmpeg and youtube-dl
-pkg update && pkg upgrade && pkg install python ffmpeg && pip install youtube-dl
+apt install -y python ffmpeg && pip install youtube-dl
 
 # setup arguments to pass to youtube-dl
 mkdir -p ~/.config/youtube-dl
@@ -17,7 +17,7 @@ cat << HERE > ~/.config/youtube-dl/config
 -o /data/data/com.termux/files/home/storage/shared/Music/Youtube/%(title)s.%(ext)s
 HERE
 
-# setup to download to share menu
+# setup to download via share menu
 mkdir ~/bin
 cat << HERE > ~/bin/termux-url-opener
 #!/bin/bash
@@ -29,3 +29,8 @@ HERE
 cat << HERE > ~/.bashrc
 pkg upgrade && pip install --upgrade pip && pip install --upgrade youtube-dl
 HERE
+
+# needed to get access to android storage
+termux-setup-storage
+
+
