2fe30933d659dfc6d78a65439ed4a1946c649cad
Kaushik <kaushik@gmail.com>
2019-09-22 20:21:52 -0400

    Upgrade pip on first install

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ apt -y update && apt -y upgrade
 apt install -y termux-api
 
 # install all necessary tools
-apt install -y git python ffmpeg atomicparsley jq && pip install youtube-dl
+apt install -y git python ffmpeg atomicparsley jq && pip install youtube-dl && pip install --upgrade pip
 
 # clone repo
 git clone https://github.com/k-a-u-s-h-i-k/termux_setup.git repo
